8fd165be73ad7f53af4e814e0605e9188c21066e
Dudcom <70006792+Dudcom@users.noreply.github.com>
2025-07-18 02:13:25 -0400

    afl++

--- procd-fuzz.c
+++ procd-fuzz.c
@@ -9,7 +9,9 @@
 #include <errno.h>
 #include <stdbool.h>
 #include <sys/socket.h>
+#include <limits.h>
 #include <json-c/json.h>
+#include <libubox/blobmsg_json.h>
 
 #include "jail/jail.h"
 #include "jail/capabilities.h"
@@ -302,3 +304,39 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
     return 0;
 }
+
+// #ifndef __AFL_FUZZ_TESTCASE_LEN
+
+// ssize_t fuzz_len;
+// unsigned char fuzz_buf[1024000];
+
+// #define __AFL_FUZZ_TESTCASE_LEN fuzz_len
+// #define __AFL_FUZZ_TESTCASE_BUF fuzz_buf  
+// #define __AFL_FUZZ_INIT() void sync(void);
+// #define __AFL_LOOP(x) \
+//     ((fuzz_len = read(0, fuzz_buf, sizeof(fuzz_buf))) > 0 ? 1 : 0)
+// #define __AFL_INIT() sync()
+
+// #endif
+
+// __AFL_FUZZ_INIT();
+
+// #pragma clang optimize off
+// #pragma GCC optimize("O0")
+
+// int main(int argc, char **argv)
+// {
+//     (void)argc; (void)argv; 
+    
+//     ssize_t len;
+//     unsigned char *buf;
+
+//     __AFL_INIT();
+//     buf = __AFL_FUZZ_TESTCASE_BUF;
+//     while (__AFL_LOOP(INT_MAX)) {
+//         len = __AFL_FUZZ_TESTCASE_LEN;
+//         LLVMFuzzerTestOneInput(buf, (size_t)len);
+//     }
+    
+//     return 0;
+// }
